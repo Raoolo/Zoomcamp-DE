5adb2841d656b5ddb4b41ef8b4ed4408f7a99f0b
Raul Gatto <gatto.raull@gmail.com>
2024-08-04 19:18:32 +0200

    Started homework

---
+++ 1c_Homework/ingest_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+
+os.system(f'wget -O green_taxi.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-01.parquet')
+os.system(f'wget -O zones.csv https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv')
+
+df = pd.read_parquet('green_taxi.parquet')
+df_zones = pd.read_csv('zones.csv')
+
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+# load data to postgres in chunks and then its done i guess
+
+# MI SONO FERMATO A CREARE UN POSTGRESQL IMAGE
---
+++ 1c_Homework/readme.md
@@ -0,0 +1,12 @@
+# Answer document
+
+## Answer to first question: --rm
+
+## Answer to second question: 
+
+docker build -t temp_image:v1
+docker run -it temp_image:v1
+pip list
+23.0.1
+
+## Answer to third question:
